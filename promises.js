e1bff7f7d1f55edebfac488fd5fe9e85cf2e0b76
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-07-10 12:03:46 +0100

    promise.all small example

--- promises.js
+++ promises.js
@@ -2,125 +2,91 @@ const readline = require("readline-sync")
 //const rp = require("request-promise");
 const request = require("request");
 
-// let long //I am still struggling with this - how to pass variable around without polluting global scope. Here, I want to bring long and lat from getLongLat into useLongLat, but couldn't.
-// let lat //ditto
-// function getLongLat (postcode, callbackAPI) {
-//     request(`https://api.postcodes.io/postcodes/${postcode}`, function (error, response, body) {
-//     //console.log('error:', error);
-//     //console.log('statusCode:', response && response.statusCode);
-//     let parsedPostcode = JSON.parse(body);
-//     long = parsedPostcode.result.longitude
-//     lat = parsedPostcode.result.latitude
-//     console.log(`Postcode: ${postcode}\nLong: ${parsedPostcode.result.longitude}\nLat: ${parsedPostcode.result.latitude}`);
-//     callbackAPI();
-//     })
-// }
-// getLongLat("M16 9PR", function (){ //here I wanted to do a callback within this anonyoous function, but couldn't make it work. in principle can you do this?
-//     request(`https://api.postcodes.io/postcodes?lon=${long}&lat=${lat}`, function (error, response, body){
-//     //console.log('error:', error);
-//     //console.log('statusCode:', response && response.statusCode);
-//     let parsedReturn = JSON.parse(body);
-//     if (parsedReturn != null) {
-//         console.log("it worked")
-//     } else {
-//         console.log("failed")
-//     }
-//     //console.log(parsedReturn.result);
-//     (!function(){
-//             console.log(`Do you feel like you are at long/lat? Y/N`);
-//             const doYouFeel = readline.prompt();
-//             if (doYouFeel === "Y"){
-//                 console.log("phew")
-//             } else (
-//                 console.log("tough nuts, kiddo")
-//             )
-//         }())
-//     })
-// })
 
 
+// 2 SIMPLE PROMISES
 
-// let promise = new Promise(function(resolve, reject) {
-//     someAsyncFunction(function callback(data, error) {
-//         if (error) { reject(error); }
-//         else { resolve(data); }
-//     });
-// });
- 
-// promise.then(function success(data) {
-//     console.log("Asynchronous function was called and returned some data: " + data);
-// }).catch(function (error) {
-//     console.log("Something went wrong");
-// });
-
-
-// // an immediately resolved promise
-// var p2 = Promise.resolve("foo"); 
-
-// // can get it after the fact, unlike events
-// p2.then((res) => console.log(res)); 
-
-// var p = new Promise(function(resolve, reject) {  
-//    setTimeout(() => resolve(4), 2000);
-// });
-
-// // handler can't change promise, just value
-// p.then((res) => {  
-//   res += 1;  
-//   console.log(res);
-// });
-
-// // still gets 4
-// p.then((res) => console.log(res));  
-
-
-// var q = new Promise(function(resolve, reject) { 
-//     for (i = 0; i < 3; i++) {
-//         if (i > 0) {
-//         resolve(console.log(i));  // fulfilled successfully
-//     }
-//         else {
-//         reject();  // error, rejected
-//     }
-//     }
-//  });
-
-// var r = new Promise((resolve, reject) => resolve(5));  
-// r.then(function(val){
-//     let newVal = val * 5
-//     console.log(newVal)
-// });
-
-// var p = new Promise((resolve, reject) => resolve(5)); //promise set up with resolve & reject
-// //we tell it to resolve right away with a value of 5 
-// p.then((val) => console.log(val)); //we then use the value of the promise with .then, and insert it into val variable with an anonymous function
-
-// request(`https://api.postcodes.io/postcodes/M169PR`, function (error, response, body) {
-//         //console.log('error:', error);
-//         //console.log('statusCode:', response && response.statusCode);
-//         let parsedPostcode = JSON.parse(body);
-//         long = parsedPostcode.result.longitude
-//         lat = parsedPostcode.result.latitude
-//         console.log(`Postcode: ${postcode}\nLong: ${parsedPostcode.result.longitude}\nLat: ${parsedPostcode.result.latitude}`);
-//         }) 
-
-
-let promise = new Promise(function(resolve, reject) { //promise starts with a function that has 2 parameters - resolve and reject
+let promise1 = new Promise(function(resolve, reject) { //promise starts with a function that has 2 parameters - resolve and reject
     if (Math.round(Math.random()) === 0) { //you can then set what you do on resolve
-        setTimeout(() => resolve("done!"), 1000)
+        setTimeout(() => resolve("promise1 done!"), 1000)
     } else { //and what you do on reject
-        setTimeout(() => reject("error..."), 1000)
+        setTimeout(() => reject("promise1 error..."), 1000)
     }
 });
 
-promise.then(
+promise1.then(
     result => console.log(result), //first argument of .then() - receives a result from a resolved promise
     error => console.log(error)  //second argument of .then() - receives an error from a rejected promise
 )
 
- 
-// promise.then(function success(data) {
-//     console.log("Asynchronous function was called and returned some data: " + data);
-// }).catch(function (error) {
-//     console.log("Something went wrong");
-// });
+let promise2 = new Promise(function(resolve, reject){
+    setTimeout(() => resolve("promise2 done!")),
+    setTimeout(() => reject("promise2 error!"))
+});
+
+promise2.then(
+    result => console.log(result),
+    error => console.log(error)
+)
+
+promise1.catch(function (error){
+    console.log("promise1 was caught")
+})
+
+promise2.catch(function (error){
+    console.log("promise2 was caught")
+})
+
+//Results - 50% of the time terminal will show:
+// promise2 done!
+// promise1 done!
+
+// //the other 50% it will show:
+// promise2 done!
+// promise1 error...
+// promise1 was caught
+
+
+//PROMISE.ALL
+
+let promise3 = new Promise(function(resolve, reject) { //promise starts with a function that has 2 parameters - resolve and reject
+    if (Math.round(Math.random()) === 0) { //you can then set what you do on resolve
+        setTimeout(() => resolve("promise3 done!"), 1000)
+    } else { //and what you do on reject
+        setTimeout(() => reject("promise3 error..."), 1000)
+    }
+});
+
+let promise4 = new Promise(function(resolve, reject){
+    setTimeout(() => resolve("promise4 done!")),
+    setTimeout(() => reject("promise4 error!"))
+});
+
+Promise.all([promise3, promise4])
+       .then(
+        result => console.log(result[0], result[1]),
+        error => console.log(`error for promise3: ${error [0]}, error for promise4: ${error[1]}`)  
+       )
+       .catch(function (error){
+        console.log("promise3 was caught")
+    })
+
+
+//Promise.all with real example
+
+let promise5 = new Promise(function(resolve, reject){
+    request(`https://api.postcodes.io/postcodes/M169PR`, function (error, response, body) {
+            //console.log('error:', error);
+            //console.log('statusCode:', response && response.statusCode);
+            let parsedPostcode = JSON.parse(body);
+            long = parsedPostcode.result.longitude
+            lat = parsedPostcode.result.latitude
+            console.log(`Postcode: ${postcode}\nLong: ${parsedPostcode.result.longitude}\nLat: ${parsedPostcode.result.latitude}`);
+            }) 
+    
+})
+
+promise5.then(
+    result => console.log(result),
+    error => console.log(error)
+)
